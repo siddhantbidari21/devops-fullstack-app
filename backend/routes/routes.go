8941b4638a7767d3b1da7c5a47944e8d7ea828b4
siddhantbidari21 <141643841+siddhantbidari21@users.noreply.github.com>
2024-11-10 19:29:02 +0530

    Update routes.go

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
-	"employees/controller"
-	"github.com/gofiber/fiber/v2"
+    "employees/controller"
+    "github.com/gofiber/fiber/v2"
 )
 
 func RegisterRoute(app *fiber.App, employeeController *controller.Employee) {
-	app.Post("/employees", employeeController.Create)
-	app.Get("/employees", employeeController.GetAll)
+    app.Post("/employees", employeeController.Create)
+    app.Get("/employees", employeeController.GetAll)
+    
+    // Adding the /health route
+    app.Get("/health", func(c *fiber.Ctx) error {
+        return c.SendString("OK")
+    })
 }
